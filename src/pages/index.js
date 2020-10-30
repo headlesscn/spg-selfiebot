759b688b937886cec8d6550d33d2dce4c123859f
weakow <w@weakow.com>
2020-10-30 20:50:29 +0800

    with Eric

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Helmet from "react-helmet"
+import { withPrefix } from "gatsby"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <Helmet>
+        <link href="https://unpkg.com/cloudinary-video-player@1.4.3/dist/cld-video-player.min.css" rel="stylesheet" />
+        <script src="https://unpkg.com/cloudinary-core@2.10.3/cloudinary-core-shrinkwrap.min.js" type="text/javascript"></script>
+        <script src="https://unpkg.com/cloudinary-video-player@1.4.3/dist/cld-video-player.min.js" type="text/javascript"></script>
+
+        <script src="https://cdn.bootcdn.net/ajax/libs/jquery/2.2.4/jquery.min.js" type="text/javascript" />
+        <script src={withPrefix('script.js')} type="text/javascript" />
+      </Helmet>
+    <SEO title="SelfieBot, Automatic Face/Body Tracking Gimbal" />
+    <div style={{
+          margin: `20px auto`,
+          textAlign: `center`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}><h2>SelfieBot</h2>
+    <p>Automatic Face\Body Tracking Gimbal</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <video controls preload="auto" style={{
+      width: `100% !important`,
+    }}>
+                  <source src='https://res.cloudinary.com/waimo/video/upload/v1604060886/SelfieBot-m_yjgaa4.mp4' />
+                </video>
+    
+    <img src="https://res.cloudinary.com/waimo/image/upload/v1604060298/en_kfpilc.png" alt="" />
   </Layout>
 )
 
---
+++ static/script.js
@@ -0,0 +1,11 @@
+const player = cld.videoPlayer('player', {
+  "fluid": true,
+  "controls": true,
+  "posterOptions": {
+      "transformation": {
+          "startOffset": "0"
+      }
+  }
+});
+
+player.source("SelfieBot-m_yjgaa4", {});
